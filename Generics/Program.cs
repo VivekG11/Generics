dba92cbdc726f91f5d5dbdead59a946caea7c925
VivekG11 <vivekvk2903@gmail.com>
2021-07-08 22:01:50 +0530

    [Creating a Generic Method to call different types of array]

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,16 +6,17 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          //Initialising types of Arrays
             int[] intArray = { 1, 3, 5, 7, 9 };
             char[] charArray = { 'G', 'E', 'N', 'E', 'R', 'I', 'C', 'S' };
             double[] doubleArray = { 11.29, 29.03, 6.03 };
 
+            //Invoking Generic methods
             Program.toPrint<int>(intArray);
             Program.toPrint<char>(charArray);
             Program.toPrint<double>(doubleArray);
         }
-
+        //Declaring a Generic Method to call types of arrays
         public static void toPrint<T>(T[] inputArray)
         {
             foreach(var element in inputArray)
