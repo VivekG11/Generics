2e3cd833491b6e1866531e57834c48b2f786d101
VivekG11 <vivekvk2903@gmail.com>
2021-07-09 10:26:55 +0530

    [Creating Generic Class ]

---
+++ Generics/GenericClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class PrintArray<T>
+    {
+        private T[] inputArray;
+
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+
+        public void toDisplay()
+        { 
+            foreach(var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("--------------------------------");
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -6,24 +6,30 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-          //Initialising types of Arrays
+
+            //Initialising types of Arrays
             int[] intArray = { 1, 3, 5, 7, 9 };
             char[] charArray = { 'G', 'E', 'N', 'E', 'R', 'I', 'C', 'S' };
             double[] doubleArray = { 11.29, 29.03, 6.03 };
 
+            //Invoking Generic classes
+            new PrintArray<int>(intArray).toDisplay();
+            new PrintArray<char>(charArray).toDisplay();
+            new PrintArray<double>(doubleArray).toDisplay();
             //Invoking Generic methods
-            Program.toPrint<int>(intArray);
-            Program.toPrint<char>(charArray);
-            Program.toPrint<double>(doubleArray);
-        }
-        //Declaring a Generic Method to call types of arrays
-        public static void toPrint<T>(T[] inputArray)
-        {
-            foreach(var element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("-----------------------------------");
+            /*  Program.toPrint<int>(intArray);
+              Program.toPrint<char>(charArray);
+              Program.toPrint<double>(doubleArray);
+          }
+          //Declaring a Generic Method to call types of arrays
+          public static void toPrint<T>(T[] inputArray)
+          {
+              foreach(var element in inputArray)
+              {
+                  Console.WriteLine(element);
+              }
+              Console.WriteLine("-----------------------------------");
+          }*/
         }
     }
 }
